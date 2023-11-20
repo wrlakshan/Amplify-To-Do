3673bb995aa989902f4701faa05a534f2d8f4008
Rashmika Lakshan <rashmika.lakshan@ebuilder.com>
2023-11-20 16:30:39 +0530

    init commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "todoapi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
